d9333067b22dad98f217e0939a9c582c28c4ff87
mrosa_san <mrosa.san.mora@gmail.com>
2021-02-24 12:31:02 +0100

    agrego un for

--- scripts.js
+++ scripts.js
@@ -2,14 +2,19 @@ let container = document.querySelector(".container");
 
 container.addEventListener("click", (evento)=> {
     if(evento.target.classList.contains("estrella")){
-        console.log("no pasa nada");
+        //console.log("no pasa nada");
     }else{
-        //window.location.reload();
-        document.getElementById("puntos-5").checked = false;
+        
+        /*document.getElementById("puntos-5").checked = false;
         document.getElementById("puntos-4").checked = false;
         document.getElementById("puntos-3").checked = false;
         document.getElementById("puntos-2").checked = false;
-        document.getElementById("puntos-1").checked = false;
+        document.getElementById("puntos-1").checked = false;*/
+        let i = 1;
+        for(i;i<=5;i++){
+            document.getElementById(`puntos-${i}`).checked = false;  
+        }
+        //document.getElementsByClassName("estrella").checked = false;
     }
 });
 
