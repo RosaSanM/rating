eec9d23a4e76bebbe7fe48182dbcffd0485c762e
mrosa_san <mrosa.san.mora@gmail.com>
2021-02-24 17:37:25 +0100

    mejoro el for

--- scripts.js
+++ scripts.js
@@ -1,20 +1,21 @@
 let container = document.querySelector(".container");
 
 container.addEventListener("click", (evento)=> {
+    
+    
     if(evento.target.classList.contains("estrella")){
-        //console.log("no pasa nada");
+        console.log("no pasa nada");
     }else{
-        
+        document.querySelectorAll('.estrella').forEach((item)=> item.checked = false);
         /*document.getElementById("puntos-5").checked = false;
         document.getElementById("puntos-4").checked = false;
         document.getElementById("puntos-3").checked = false;
         document.getElementById("puntos-2").checked = false;
-        document.getElementById("puntos-1").checked = false;*/
+        document.getElementById("puntos-1").checked = false;
         let i = 1;
         for(i;i<=5;i++){
             document.getElementById(`puntos-${i}`).checked = false;  
-        }
-        //document.getElementsByClassName("estrella").checked = false;
+        }*/
     }
 });
 
